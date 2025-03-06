8d7da83f453f8a53b9bc733fd1585fb30107596d
Radiant Fadilah <radiantfadilah0@gmail.com>
2025-03-06 11:40:37 +0700

    [update] styling

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,19 +1,13 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput as DefaultInput,
-  TextInputProps,
-} from "react-native";
-import React from "react";
+import { View, TextInput as DefaultInput, TextInputProps } from "react-native";
 import { useAppSelector } from "@/hooks";
 
 export default function TextInput({ style, ...rest }: TextInputProps) {
   const colors = useAppSelector((state) => state.settings.theme.colors);
+
   return (
     <View
       style={{
-        backgroundColor: colors.input,
+        backgroundColor: colors[200],
         minHeight: 45,
         maxHeight: 100,
         shadowRadius: 3,
@@ -28,19 +22,17 @@ export default function TextInput({ style, ...rest }: TextInputProps) {
         style={[
           {
             minHeight: 45,
-            color: colors.text,
-            fontFamily: "ManropeRegular",
+            color: colors[900],
+            fontFamily: "PJKTSansRegular",
             paddingHorizontal: 20,
             paddingVertical: 12.5,
             borderRadius: 45 / 2,
           },
           style,
         ]}
-        placeholderTextColor={colors.input_placeholder}
+        placeholderTextColor={colors[400]}
         {...rest}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
